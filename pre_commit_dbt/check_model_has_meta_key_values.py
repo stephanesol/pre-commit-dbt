566abc99ba377dd9486c106c3b53695544957fed
stephanesol <stephanesol@users.noreply.github.com>
2023-05-17 18:35:47 -0500

    add test

---
+++ pre_commit_dbt/check_model_has_meta_key_values.py
@@ -0,0 +1,100 @@
+import argparse
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Sequence
+
+from pre_commit_dbt.utils import add_filenames_args
+from pre_commit_dbt.utils import add_manifest_args
+from pre_commit_dbt.utils import get_filenames
+from pre_commit_dbt.utils import get_json
+from pre_commit_dbt.utils import get_model_schemas
+from pre_commit_dbt.utils import get_model_sqls
+from pre_commit_dbt.utils import get_models
+from pre_commit_dbt.utils import get_disabled_models
+from pre_commit_dbt.utils import JsonOpenError
+
+
+def has_meta_key(
+    paths: Sequence[str], manifest: Dict[str, Any], meta_key: str, meta_key_values: Sequence[str]
+) -> int:
+    status_code = 0
+    ymls = get_filenames(paths, [".yml", ".yaml"])
+    sqls = get_model_sqls(paths, manifest)
+    filenames = set(sqls.keys())
+
+    # get manifest nodes that pre-commit found as changed
+    models = get_models(manifest, filenames)
+
+    disabled_models = get_disabled_models(manifest, filenames)
+
+    # if user added schema but did not rerun the model
+    schemas = get_model_schemas(list(ymls.values()), filenames)
+
+    # convert to sets
+    model_key_value_dict = {}
+
+    in_models = set()
+    for model in models:
+        key_value = set(model.node.get("meta", {}).get(meta_key, {}))
+        model_key_value_dict[model.filename] = key_value
+        if set(key_value).issubset(meta_key_values):
+            in_models.add(model.filename)
+
+    in_disabled = set()
+    for model in disabled_models:
+        in_disabled.add(model.filename)
+
+    in_schemas = set()
+    for schema in schemas:
+        key_value = set(schema.schema.get("meta", {}).get(meta_key, {}))
+        if model_key_value_dict.get(schema.model_name, None):
+            model_key_value_dict[schema.model_name].update(model_key_value_dict[schema.model_name].update(key_value))
+        else:
+            model_key_value_dict[schema.model_name] = key_value
+
+        if set(key_value).issubset(meta_key_values):
+            in_schemas.add(schema.model_name)
+
+    missing = filenames.difference(in_models, in_schemas, in_disabled)
+
+    for model in missing:
+        status_code = 1
+        model_keys = model_key_value_dict.get(model,set())
+        missing_keys = set(meta_key_values).difference(model_keys) if model_keys else meta_key_values
+        result = "\n- ".join(list(missing_keys))  # pragma: no mutate
+        print(
+            f"{sqls.get(model)}: "
+            f"the following values are no valid for the key {meta_key}:\n- {result}",
+            f"must be one of {meta_key_values}",
+        )
+    return status_code
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    add_filenames_args(parser)
+    add_manifest_args(parser)
+
+    parser.add_argument(
+        "--meta-keys",
+        nargs="+",
+        required=True,
+        help="List of required key in meta part of model.",
+    )
+
+    args = parser.parse_args(argv)
+
+    try:
+        manifest = get_json(args.manifest)
+    except JsonOpenError as e:
+        print(f"Unable to load manifest file ({e})")
+        return 1
+
+    return has_meta_key(
+        paths=args.filenames, manifest=manifest, meta_keys=args.meta_keys
+    )
+
+
+if __name__ == "__main__":
+    exit(main())
